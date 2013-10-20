b804172a7bf6efa9c9e9237de35c7e9d6e18641d
Ryan Puffer <pufferry@gmail.com>
2013-10-20 00:09:06 -0400

    Start of subnet and tweeter

--- src/main/java/com/sf/codingcomp/subnetutilities/SubnetImpl.java
+++ src/main/java/com/sf/codingcomp/subnetutilities/SubnetImpl.java
@@ -1,12 +1,38 @@
 package com.sf.codingcomp.subnetutilities;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.List;
 
 public class SubnetImpl implements Subnet {
+	
+	public static void main(String[] args) throws UnknownHostException, InvalidMaskException {
+		new SubnetImpl(InetAddress.getByName("10.0.0.0"), 24);
+	}
+	
+	private byte[] subnet;
+	private int mask;
+	private InetAddress original;
+	
 	public SubnetImpl(InetAddress subnet, int mask) throws InvalidMaskException {
-		// TODO Auto-generated method stub
-
+		if (mask < 1 || mask > 32) // bro are you even trying? 1-32 or no networking for you.
+			throw new InvalidMaskException();
+		
+		this.original = subnet;
+		this.subnet = subnet.getAddress();
+		this.mask = mask;
+		
+		int maskdiff = 32 - mask;
+		
+		// adjust for subnet
+		for (int i = 3; i >= 0 && maskdiff > 0; i--)
+		{
+			int thisiter = maskdiff % 9;
+			this.subnet[i] -= Math.pow(2,thisiter);
+			maskdiff -= 8;
+		}
+		
+		
 	}
 	
 	public boolean isIPInRange(InetAddress ipAddress) {
@@ -16,7 +42,11 @@ public class SubnetImpl implements Subnet {
 
 	public InetAddress getNetworkAddress() {
 		// TODO Auto-generated method stub
-		return null;
+		try {
+			return InetAddress.getByAddress(subnet);
+		} catch (UnknownHostException e) {
+			return original;
+		}
 	}
 
 	public InetAddress getBroadcastAddress() {
@@ -30,7 +60,6 @@ public class SubnetImpl implements Subnet {
 	}
 
 	public int getAddressCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return (int)Math.pow(2, 32 - mask);
 	}	
 }
--- src/main/java/com/sf/codingcomp/tweet/User.java
+++ src/main/java/com/sf/codingcomp/tweet/User.java
@@ -1,9 +1,14 @@
 package com.sf.codingcomp.tweet;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class User {
 
 	public String username;
 	public Feed feed = new Feed();
+	public List<User> followedBy = new ArrayList<User>();
+	public List<User> follows = new ArrayList<User>();
 
 	public String getUsername() {
 		return username;
@@ -22,15 +27,21 @@ public class User {
 	}
 
 	public void follow(User user) {
-		// TODO implement me
+		follows.add(user);
+		user.followedBy.add(this);
 	}
 
 	public void unfollow(User user) {
-		// TODO implement me
+		follows.remove(user);
+		user.followedBy.remove(this);
 	}
 
 	public String toString() {
 		return username;
+	}
+	
+	public List<User> getFollowers() {
+		return followedBy;
 	}
 
 }
