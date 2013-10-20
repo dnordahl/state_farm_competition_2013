5ce7e6e0ecce81fa3b24ef09daf96cf15f0d3532
Ryan Puffer <pufferry@gmail.com>
2013-10-19 22:00:23 -0400

    Ducks managed

--- src/main/java/com/sf/codingcomp/duck/DuckFinder.java
+++ src/main/java/com/sf/codingcomp/duck/DuckFinder.java
@@ -1,11 +1,32 @@
 package com.sf.codingcomp.duck;
 
+import java.lang.reflect.Method;
+
 public class DuckFinder {
 
 	public static <T> boolean isADuck(T objectToCheck) {
 
-		// TODO implement me
-		return false;
+		Method[] methods = objectToCheck.getClass().getMethods();
+		
+		// find looksLikeADuck()
+		boolean looks = false;
+		for (Method m : methods) {
+			if ("looksLikeADuck".equals(m.getName())) {
+				looks = true;
+				break;
+			}
+		}
+		
+		
+		boolean quacks = false;
+		for (Method m : methods) {
+			if ("quacksLikeADuck".equals(m.getName())) {
+				quacks = true;
+				break;
+			}
+		}
+		
+		return looks && quacks;
 	}
 
 }
