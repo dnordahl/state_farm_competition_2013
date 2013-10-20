566277cf33401a1433bd67d8a24c94ccf011e500
Ryan Puffer <pufferry@gmail.com>
2013-10-19 21:49:38 -0400

    Started regex

--- src/main/java/com/sf/codingcomp/regex/Regex.java
+++ src/main/java/com/sf/codingcomp/regex/Regex.java
@@ -1,5 +1,8 @@
 package com.sf.codingcomp.regex;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Regex {
 	/**
 	 * 10 numbers.
@@ -8,8 +11,9 @@ public class Regex {
 	 * @return
 	 */
 	public static boolean isFullPhoneNumber(String phone) {
-		// TODO: Implement this method.
-		return false;
+		Pattern phoneRegex = Pattern.compile("\\d{3}(-| ){0,1}\\d{3}(-| ){0,1}\\d{4}");
+		Matcher phoneMatch = phoneRegex.matcher(phone);
+		return phoneMatch.matches();
 	}
 
 	/**
@@ -20,14 +24,22 @@ public class Regex {
 	 * @return
 	 */
 	public static boolean isAmericanAddress(String address) {
-
-		// TODO: Implement this method.
-		return false;
+		String street = "(\\w+( \\w+)*( \\d+){0,1})|(PO Box \\d+)";
+		String city = "(\\w+( \\w+)*)";
+		String state = "[A-Z]{2}";
+		String zip = "\\d{5}(-\\d{4}|\\d{4}){0,1}";
+		
+		String delimiter = ",{0,1} +";
+		
+		Pattern addressRegex = Pattern.compile(street + delimiter + city + delimiter + state + zip);
+		Matcher addressMatch = addressRegex.matcher(address);
+		return addressMatch.matches();
 	}
 
 	public static boolean isIPv4Address(String ip) {
-
-		// TODO: Implement this method.
-		return false;
+		// pattern from http://www.regular-expressions.info/examples.html
+		Pattern ipRegex = Pattern.compile("(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
+		Matcher ipMatch = ipRegex.matcher(ip);
+		return ipMatch.matches();
 	}
 }
